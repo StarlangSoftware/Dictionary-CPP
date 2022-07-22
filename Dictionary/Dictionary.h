a5dd0bbb645d6f7075c9dfab9a4796595181b9a8
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-07-22 22:20:21 +0300

    Moved turkishWordComparator from Dictionary.cpp to Dictionary.h

--- Dictionary/Dictionary.h
+++ Dictionary/Dictionary.h
@@ -37,5 +37,62 @@ public:
     unsigned long getWordStartingWith(string hash);
 };
 
+struct turkishWordComparator{
+    map<string, int> comparatorMap;
+/**
+ * The wordComparator takes two {@link Word}s as inputs; wordA and wordB and compares their names. Returns the result of this comparison.
+ *
+ * @param wordA Word type input.
+ * @param wordB Word type input.
+ * @return the value {@code 0} if the wordA is equal to the wordB; a value less than {@code 0} if this wordA is
+ * lexicographically less than wordB; and a value greater than {@code 0} if this wordA is lexicographically greater
+ * than wordB.
+ */
+    turkishWordComparator(map<string, int> comparatorMap){
+        this->comparatorMap = comparatorMap;
+    }
+
+    bool operator() (Word* wordA, Word* wordB){
+        const char* charPtr1 = wordA->getName().c_str();
+        const char* charPtr2 = wordB->getName().c_str();
+        if (wordA->getName() == wordB->getName()){
+            return false;
+        }
+        if (wordB->getName().starts_with(wordA->getName())){
+            return true;
+        }
+        if (wordA->getName().starts_with(wordB->getName())){
+            return false;
+        }
+        while (*charPtr1 && *charPtr2){
+            string char1;
+            if ((*charPtr1 & 0xC0) != 0x80){
+                do{
+                    char1 += *charPtr1;
+                    charPtr1++;
+                } while ((*charPtr1 & 0xC0) == 0x80);
+            }
+            string char2;
+            if ((*charPtr2 & 0xC0) != 0x80){
+                do{
+                    char2 += *charPtr2;
+                    charPtr2++;
+                } while ((*charPtr2 & 0xC0) == 0x80);
+            }
+            if (comparatorMap.contains(char1) && comparatorMap.contains(char2)){
+                int index1 = comparatorMap.at(char1), index2 = comparatorMap.at(char2);
+                if (index1 < index2){
+                    return true;
+                } else {
+                    if (index1 > index2){
+                        return false;
+                    }
+                }
+            } else {
+                return true;
+            }
+        }
+    }
+};
 
 #endif //DICTIONARY_DICTIONARY_H
